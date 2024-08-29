d1f1b8f3fbfe2028da49e479fd1374f083bdf49e
Emre <celenemre7@gmail.com>
2024-08-29 18:40:52 +0300

    movie id

---
+++ app/movie/[id]/page.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Movies from '@/mocks/movies.json'
+import MovieContainer from '@/containers/movie'
+import { notFound } from 'next/navigation'
+
+
+function MoviePage( {params, searchParams} ) {
+
+    const movieDetail = Movies.results.find(movie => movie.id.toString() === params.id)
+
+    if (!movieDetail) {
+        notFound()
+    }
+
+    if (searchParams.error === 'true') {
+        throw new Error('Error Happened')
+    }
+  return (
+    <div>
+        <MovieContainer movie={movieDetail} />
+    </div>
+  )
+}
+
+export default MoviePage
--- components/categories/index.jsx
+++ components/categories/index.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import Link from 'next/link'
 import styles from '@/components/categories/styles.module.css'
-function Categories({categories}) {
+
+function Categories({ categories = [] }) {
+  if (!categories || categories.length === 0) {
+    return <div>Kategoriler bulunamadı.</div>;
+  }
+
   return (
     <div className={styles.categories}>
-        {categories.map((category) => ( 
-          <Link key={category.id} className='styles.category' href={`/${category.id}`}>
-              <div className={styles.name}>{category.name}    </div>
-          </Link>
-))}
-
+      {categories.map((category) => (
+        <Link key={category.id} className={styles.category} href={`/${category.id}`}>
+          <div className={styles.name}>{category.name}</div>
+        </Link>
+      ))}
     </div>
-  )
+  );
 }
 
 export default Categories;
