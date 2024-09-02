b6ec3d1660326a655f053cc510f8a629069e3cc2
Emre <celenemre7@gmail.com>
2024-09-02 17:50:13 +0300

    services

--- app/[[...category]]/page.jsx
+++ app/[[...category]]/page.jsx
@@ -1,56 +1,25 @@
 import React from "react";
 import HomeContainer from "@/containers/home"
-
-const API_URL = "https://api.themoviedb.org/3";
-
-const getTopRatedMovies = async () => {
-  const res = await fetch(
-    `${API_URL}/movie/top_rated?api_key=${process.env.API_KEY}&page=1`
-  );
-  
-  return res.json()
-};
-
-
-const getPopularMovies = async () => {
-  const res = await fetch (
-    `${API_URL}/movie/popular?api_key=${process.env.API_KEY}&page=1`
-
-  )
-
-  return res.json()
-}
-
-const getCategories = async () => {
-  const res = await fetch (
-    `${API_URL}/genre/movie/list?api_key=${process.env.API_KEY}&page=1`
-  )
-  return res.json()
-}
-
-const getSingleCategorty = async (genreId) => {
-  const res = await fetch (
-    `${API_URL}/discover/movie?api_key=${process.env.API_KEY}&with_genres=${genreId}`
-  )
-  return res.json()
-}
+import { fetchMovieApi } from "@/services/movie";
+import {
+  getSingleCategory,
+  getCategories,
+  getPopularMovies,
+  getTopRatedMovies,
+} from '@/services/movie'
 
 async function HomePage({params}) {
   let selectedCategory;
 
-  const topRatedPromise = getTopRatedMovies()
-  const popularPromise = getPopularMovies()
-  const categoryPromise = getCategories()
-
   const [{ results: topRatedMovies }, { results: popularMovies }, {genres: categories}] = await Promise.all([
-    topRatedPromise,
-    popularPromise,
-    categoryPromise
+    getTopRatedMovies(),
+    getPopularMovies(),
+    getCategories()
   ]);
 
   if (params.category?.length > 0) {
     
-    const results = await getSingleCategorty(params.category[0]);
+    const results = await getSingleCategory(params.category[0]);
     selectedCategory = results
   }
   
--- app/movie/[id]/page.jsx
+++ app/movie/[id]/page.jsx
@@ -1,29 +1,13 @@
 import React from 'react'
 import MovieContainer from '@/containers/movie'
 import { notFound } from 'next/navigation'
-
-
-const API_URL = "https://api.themoviedb.org/3";
-
-const getMovie = async (movieId) => {
-  const res = await fetch(
-    `${API_URL}/movie/${movieId}?api_key=${process.env.API_KEY}`
-  );
-  return res.json();
-};
-
-
-
+import { getMovie } from '@/services/movie'
 async function MoviePage( {params, searchParams} ) {
 
     const movieDetail = await getMovie(params.id)
 
     if (!movieDetail) {
         notFound()
-    }
-
-    if (searchParams.error === 'true') {
-        throw new Error('Error Happened')
     }
     
   return (
