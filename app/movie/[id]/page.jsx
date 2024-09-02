d8d3397814160b62e4fa9e8c06caf2c0f39354f1
Emre <celenemre7@gmail.com>
2024-09-02 17:18:47 +0300

    movie page backend

--- app/movie/[id]/page.jsx
+++ app/movie/[id]/page.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import Movies from '@/mocks/movies.json'
 import MovieContainer from '@/containers/movie'
 import { notFound } from 'next/navigation'
 
 
-function MoviePage( {params, searchParams} ) {
+const API_URL = "https://api.themoviedb.org/3";
 
-    const movieDetail = Movies.results.find(movie => movie.id.toString() === params.id)
+const getMovie = async (movieId) => {
+  const res = await fetch(
+    `${API_URL}/movie/${movieId}?api_key=${process.env.API_KEY}`
+  );
+  return res.json();
+};
+
+
+
+async function MoviePage( {params, searchParams} ) {
+
+    const movieDetail = await getMovie(params.id)
 
     if (!movieDetail) {
         notFound()
@@ -15,6 +25,7 @@ function MoviePage( {params, searchParams} ) {
     if (searchParams.error === 'true') {
         throw new Error('Error Happened')
     }
+    
   return (
     <div>
         <MovieContainer movie={movieDetail} />
